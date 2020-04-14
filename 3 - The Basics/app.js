07fbd4aee7b8d459a48caa195e60d11abb93d192
Ed Boorn <43168926+edboorn@users.noreply.github.com>
2020-04-14 14:34:28 +0100

    Added basic routing

--- 3 - The Basics/app.js
+++ 3 - The Basics/app.js
@@ -1,12 +1,24 @@
 const http = require("http");
 
-const server = http.createServer((req,res) => {
-    console.log(req.url, req.method, req.headers);
-    //process.exit();
-    res.setHeader('Content-Type' , 'text/html')
-    res.write('<html>');
-    res.write('<h1>Hello from the other side</h1>')
-    res.write('</html>');
+const server = http.createServer((req, res) => {
+  const url = req.url;
+
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"> <input type="text" name="message"> <button type="submit">Submit</button> </form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title> Hello from Nodejs </title></head>");
+  res.write("<body><h1> This is the first page </h1> </body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
