5425d0b138fae22af4c460bddbf413bcb2fed34a
Ed Boorn <43168926+edboorn@users.noreply.github.com>
2020-04-14 14:12:14 +0100

    slight server refactor
    Removed the seperate funciton that was handling the request data and moved in the .createServer

--- 3 - The Basics/app.js
+++ 3 - The Basics/app.js
@@ -1,9 +1,12 @@
 const http = require("http");
 
-const rqListener = (req, res) => {
-    console.log(req)
-};
-
- const server = http.createServer(rqListener);
+const server = http.createServer((req,res) => {
+    console.log(req.url, req.method, req.headers);
+    //process.exit();
+    res.setHeader('Content-Type' , 'text/html')
+    res.write('<html>');
+    res.write('<h1>Hello from the other side</h1>')
+    res.write('</html>');
+});
 
 server.listen(3000);
