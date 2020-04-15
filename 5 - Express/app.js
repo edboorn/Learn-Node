2abf0742d8aafcb0b823fad6d3e350997b6ea28f
Ed Boorn <43168926+edboorn@users.noreply.github.com>
2020-04-15 16:38:52 +0100

    initial setup and express

---
+++ 5 - Express/app.js
@@ -0,0 +1,16 @@
+const http = require("http");
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('In the middleware')
+    next(); // Allows the call to go to the next bit of middleware
+});
+
+app.use((req, res, next) => {
+    console.log('In the middleware')
+    res.send('<h1> Hello from express </h1>');
+});
+
+app.listen(3000);
