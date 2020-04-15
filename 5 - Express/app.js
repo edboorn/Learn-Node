6dd58ac2ae61b1bb67c9df491a302c0717df8c5a
Ed Boorn <43168926+edboorn@users.noreply.github.com>
2020-04-15 16:46:22 +0100

    added basic middleware

--- 5 - Express/app.js
+++ 5 - Express/app.js
@@ -1,13 +1,12 @@
 const express = require("express");
 
 const app = express();
-
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
     console.log('In the middleware')
-    next(); // Allows the call to go to the next bit of middleware
+    res.send('<h1> The add product page </h1>');
 });
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
     console.log('In the middleware')
     res.send('<h1> Hello from express </h1>');
 });
