9ee909c993c5a1280504a40492ef97a6ad95feb5
Ed Boorn <43168926+edboorn@users.noreply.github.com>
2020-04-16 14:26:52 +0100

    Added admin routes

--- 5 - Express/app.js
+++ 5 - Express/app.js
@@ -10,14 +10,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({extended : true}));
 
 // Route handling middlewares
-app.use(adminRoutes);
+app.use('/admin',adminRoutes); //All admin routes now start with /admin
 app.use(shopRoutes);
 
 
 //Catchall 404 page not found error
 app.use((req,res,next) => { 
     //Can chain a bunch of different res. methods, send just has to be the last
-    res.status(404).send('<h1> Page Not Found </h1><form action="/add-product" method="GET"><button type="submit">Return to Add Product</button></form>');
+    res.status(404).send('<h1> Page Not Found </h1><form action="/admin/add-product" method="GET"><button type="submit">Return to Add Product</button></form>');
 });
 
 app.listen(3000);
--- 5 - Express/routes/admin.js
+++ 5 - Express/routes/admin.js
@@ -4,12 +4,12 @@ const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
   res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"> <button type="submit">Add Product</button></form>'
+    '<form action="/admin/add-product" method="POST"><input type="text" name="title"> <button type="submit">Add Product</button></form>'
   );
 });
 
 // Filters the url to only show for post requests
-router.post("/product", (req, res, next) => {
+router.post("/add-product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
