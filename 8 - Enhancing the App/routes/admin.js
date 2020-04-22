281bbc2bc6a53e2a00b30518e02d9fa71d0ab067
Ed Boorn <43168926+edboorn@users.noreply.github.com>
2020-04-22 11:45:10 +0100

    Added the files

---
+++ 8 - Enhancing the App/routes/admin.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+
+const productsController = require('../controllers/products')
+
+
+router.get("/add-product", productsController.getAddProduct );
+
+// Filters the url to only show for post requests
+router.post("/add-product", productsController.postAddProduct);
+
+module.exports = router;
