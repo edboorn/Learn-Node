49e35cf87b8ba13e21f812440bed08bc2c315f60
Ed Boorn <43168926+edboorn@users.noreply.github.com>
2020-04-22 21:42:00 +0100

    Secuirty fix and inital cart creation

--- 9 - Dynamic Routes and Advance Models/controllers/shop.js
+++ 9 - Dynamic Routes and Advance Models/controllers/shop.js
@@ -10,6 +10,17 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
+exports.getProduct = (req,res,next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    res.render('shop/product-detail', {
+      product : product,
+      pageTitle : product.title,
+      path : '/products'
+    })
+  })
+}
+
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('shop/index', {
@@ -27,6 +38,11 @@ exports.getCart = (req, res, next) => {
   });
 };
 
+exports.postCart =(req,res,next) => {
+  const prodId = req.body.productId
+  console.log(prodId)
+  res.redirect("/cart")
+}
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
     path: '/orders',
