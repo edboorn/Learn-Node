4fb6c5c992f99722229a4faff03ac39e7b574eeb
Edward Boorn <edward.boorn@btinternet.com>
2022-03-04 12:49:01 +0000

    implemented sql database link

--- 9 - Dynamic Routes and Advance Models/controllers/shop.js
+++ 9 - Dynamic Routes and Advance Models/controllers/shop.js
@@ -2,12 +2,16 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll()
+  .then(([rows, fieldData]) => {
     res.render('shop/product-list', {
-      prods: products,
+      prods: rows,
       pageTitle: 'All Products',
       path: '/products'
     });
+  })
+  .catch(err => {
+    console.log(err)
   });
 };
 
@@ -23,13 +27,15 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll()
+  .then(([rows, fieldData]) => {
     res.render('shop/index', {
-      prods: products,
+      prods: rows,
       pageTitle: 'Shop',
       path: '/'
     });
-  });
+  })
+  .catch(err => {console.log(err)});
 };
 
 exports.getCart = (req, res, next) => {
